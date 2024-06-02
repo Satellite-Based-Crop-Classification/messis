255215a21073a57f10f71ff94fc821b834836f72
Florin Barbisch <40719099+florinbarbisch@users.noreply.github.com>
2024-06-02 22:03:47 +0200

    Added: use a separate fold for testing

--- messis/dataloader.py
+++ messis/dataloader.py
@@ -5,6 +5,7 @@ from torchvision import transforms
 from pytorch_lightning import LightningDataModule
 
 import os
+import re
 import yaml
 import rasterio
 
@@ -46,27 +47,25 @@ def get_mask_transforms():
     return transforms.Compose([])
 
 class GeospatialDataset(Dataset):
-    def __init__(self, data_dir, test_fold, train=True, transform_img=None, transform_mask=None, transform_field_ids=None, crop_to=None, debug=False, subset_size=None):
+    def __init__(self, data_dir, fold_indicies, transform_img=None, transform_mask=None, transform_field_ids=None, debug=False, subset_size=None):
         self.data_dir = data_dir
         self.chips_dir = os.path.join(data_dir, 'chips')
         self.transform_img = transform_img
         self.transform_mask = transform_mask
         self.transform_field_ids = transform_field_ids
-        self.crop_to = crop_to
         self.debug = debug
-        self.train = train
         self.images = []
         self.masks = []
         self.field_ids = []
 
-        self.means, self.stds = self.load_stats(test_fold)
+        self.means, self.stds = self.load_stats(fold_indicies, 3) # Hardcoded 3 timesteps for now
         self.transform_img_load = self.get_img_load_transforms(self.means, self.stds)
         self.transform_mask_load = self.get_mask_load_transforms()
         self.transform_field_ids_load = self.get_field_ids_load_transforms()
         
         # Adjust file selection based on fold
         for file in os.listdir(self.chips_dir):
-            if file.endswith("_merged.tif") and f"_fold_{test_fold}" not in file if train else file.endswith("_merged.tif") and f"_fold_{test_fold}" in file:
+            if re.match(f".*_fold_[{"".join(fold_indicies)}]_merged.tif", file):
                 self.images.append(file)
                 mask_file = file.replace("_merged.tif", "_mask.tif")
                 self.masks.append(mask_file)
@@ -83,7 +82,7 @@ class GeospatialDataset(Dataset):
             self.masks = [self.masks[i] for i in selected_indices]
             self.field_ids = [self.field_ids[i] for i in selected_indices]
 
-    def load_stats(self, test_fold):
+    def load_stats(self, fold_indicies, n_timesteps=3):
         """Load normalization statistics for dataset from YAML file."""
         stats_path = os.path.join(self.data_dir, 'chips_stats.yaml')
         if self.debug:
@@ -91,12 +90,30 @@ class GeospatialDataset(Dataset):
         assert os.path.exists(stats_path), f"mean/std stats file for dataset not found at {stats_path}"
         with open(stats_path, 'r') as file:
             stats = yaml.safe_load(file)
-        key = f'fold_{test_fold}'
-        if key not in stats:
-            raise ValueError(f"mean/std stats for fold {test_fold} not found in {stats_path}")
-        if self.debug:
-            print(f"Stats with selected test fold {test_fold}: {stats[key]} over {stats[key]['n_timesteps']} timesteps.")
-        return stats[key]['mean'] * stats[key]['n_timesteps'], stats[key]['std'] * stats[key]['n_timesteps']
+        mean_list, std_list, n_list = [], [], []
+        for fold in fold_indicies:
+            key = f'fold_{fold}'
+            if key not in stats:
+                raise ValueError(f"mean/std stats for fold {fold} not found in {stats_path}")
+            if self.debug:
+                print(f"Stats with selected test fold {fold}: {stats[key]} over {n_timesteps} timesteps.")
+            mean_list.append(stats[key]['mean']) # list of 6 means
+            std_list.append(stats[key]['std']) # list of 6 stds
+            n_list.append(stats[key]['n_chips']) # list of 6 ns
+        # aggregate means and stds over all folds
+        means, stds = [], []
+        for channel in range(mean_list[0].shape[0]):
+            means.append(torch.stack([mean_list[i][channel] for i in range(len(mean_list))]).mean())
+            # stds are waaaay more complex to aggregate
+            # \sqrt{\frac{\sum_{i=1}^{n} (\sigma_i * (n_i - 1))}{\sum_{i=1}^{n} (n_i) - n}}
+            variances = torch.stack([std_list[i][channel] ** 2 for i in range(len(std_list))])
+            n = torch.tensor([n_list[i] for i in range(len(n_list))], dtype=torch.float32)
+            combined_variance = torch.sum(variances * (n - 1)) / (torch.sum(n) - len(n_list))
+            stds.append(torch.sqrt(combined_variance))
+
+        # make means and stds into 2d arrays, as torchvision would otherwise convert it into a 3d tensor which is incompatible with our 4d temporal images
+        # https://github.com/pytorch/vision/blob/6e18cea3485066b7277785415bf2e0422dbdb9da/torchvision/transforms/_functional_tensor.py#L923
+        return means * n_timesteps, stds * n_timesteps
 
     def get_img_load_transforms(self, means, stds):
         return transforms.Compose([
@@ -132,19 +149,6 @@ class GeospatialDataset(Dataset):
         mask = self.transform_mask_load(mask)
         field_ids = self.transform_field_ids_load(field_ids)
         
-
-        # Crop the image and mask if applicable
-        if self.crop_to:
-            if self.train:
-                i, j, h, w = transforms.RandomCrop.get_params(img, output_size=(self.crop_to, self.crop_to))
-                img = transforms.functional.crop(img, i, j, h, w)
-                mask = transforms.functional.crop(mask, i, j, h, w)
-                field_ids = transforms.functional.crop(field_ids, i, j, h, w)
-            else:
-                img = transforms.functional.center_crop(img, self.crop_to)
-                mask = transforms.functional.center_crop(mask, self.crop_to)
-                field_ids = transforms.functional.center_crop(field_ids, self.crop_to)
-
         # Apply additional transforms passed from module if applicable
         if self.transform_img is not None:
             img = self.transform_img(img)
@@ -160,33 +164,41 @@ class GeospatialDataset(Dataset):
         return img, ((targets_tier1, targets_tier2, targets_tier3), field_ids)
 
 class GeospatialDataModule(LightningDataModule):
-    def __init__(self, data_dir, test_fold, batch_size=8, num_workers=4, crop_to=None, debug=False, subsets=None):
+    def __init__(self, data_dir, train_folds, val_folds, test_folds, batch_size=8, num_workers=4, debug=False, subsets=None):
         super().__init__()
         self.data_dir = data_dir
-        self.test_fold = test_fold
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.crop_to = crop_to
         self.debug = debug
         self.subsets = subsets if subsets is not None else {}
+        
+        GeospatialDataModule.validate_folds(train_folds, val_folds, test_folds)
+        self.train_folds = train_folds
+        self.val_folds = val_folds
+        self.test_folds = test_folds
 
         # NOTE: Transforms on this level not used for now
         self.transform_img = get_img_transforms()
         self.transform_mask = get_mask_transforms()
 
+    @staticmethod
+    def validate_folds(train, val, test):
+        if train is None or val is None or test is None:
+            raise ValueError("All fold sets must be specified")
+        
+        if len(set(train) & set(val)) > 0 or len(set(train) & set(test)) > 0 or len(set(val) & set(test)) > 0:
+            raise ValueError("Folds must be mutually exclusive")
+
     def setup(self, stage=None):
         common_params = {
             'data_dir': self.data_dir,
-            'test_fold': self.test_fold,
-            'crop_to': self.crop_to,
             'debug': self.debug,
         }
         if stage in ('fit', None):
-            self.train_dataset = GeospatialDataset(train=True,  subset_size=self.subsets.get('train', None), **common_params)
-            self.val_dataset   = GeospatialDataset(train=False, subset_size=self.subsets.get('val',   None), **common_params)
+            self.train_dataset = GeospatialDataset(fold_indicies=self.train_folds, subset_size=self.subsets.get('train', None), **common_params)
+            self.val_dataset   = GeospatialDataset(fold_indicies=self.val_folds,   subset_size=self.subsets.get('val',   None), **common_params)
         if stage in ('test', None):
-            # TODO: Fix this, we should not be using the same dataset for val and test
-            self.test_dataset  = GeospatialDataset(train=False, subset_size=self.subsets.get('test',  None), **common_params)
+            self.test_dataset  = GeospatialDataset(fold_indicies=self.test_folds,  subset_size=self.subsets.get('test',  None), **common_params)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, persistent_workers=True, shuffle=True)
