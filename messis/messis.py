9988e602081ed74a9a7100fd0b74885a7d6ef300
Yvo Keller <keller.yvo@gmail.com>
2024-06-22 18:08:19 +0200

    top level if condition in __update_per_class_accuracy

--- messis/messis.py
+++ messis/messis.py
@@ -680,6 +680,9 @@ class LogMessisMetrics(pl.Callback):
 
     def __update_per_class_accuracy(self, preds, targets, per_class_accuracies):
         for class_index, class_accuracy in per_class_accuracies.items():
+            if not (targets == class_index).any():
+                continue
+            
             if class_index == 0:
                 # Mask out non-background elements for background class (0)
                 background_mask = targets != 0
@@ -694,10 +697,9 @@ class LogMessisMetrics(pl.Callback):
             preds_class = (preds_fields == class_index).float()
             targets_class = (targets_fields == class_index).float()
 
-            if targets_class.any():
-                class_accuracy.update(preds_class, targets_class)
-                if self.debug:
-                    print(f"Per-class accuracy for class {class_index} updated. Update count: {per_class_accuracies[class_index]._update_count}")
+            class_accuracy.update(preds_class, targets_class)
+            if self.debug:
+                print(f"Per-class accuracy for class {class_index} updated. Update count: {per_class_accuracies[class_index]._update_count}")
 
     def on_train_epoch_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule):
         self.__on_epoch_end(trainer, pl_module, 'train')
